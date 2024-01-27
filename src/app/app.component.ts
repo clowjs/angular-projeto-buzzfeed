b50baf1e68324877b8c116bfd7f2f6459c8b149a
Sergio Jose dos Santos Junior <hagare@gmail.com>
2024-01-27 20:06:47 -0300

    estrutura base criada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuizComponent } from './components/quiz/quiz.component'
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuizComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
